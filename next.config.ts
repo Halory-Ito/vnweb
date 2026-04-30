68160c298514f6bdfc884fec6c1a5f577ee0846e
Halory <2082004203@qq.com>
2026-04-30 10:51:33 +0800

    fixed: 优化了打包体积

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,34 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    // 自动按需引入第三方包，大幅缩小打包体积
+    optimizePackageImports: [
+      'lucide-react',
+      'date-fns',
+      '@radix-ui/react-icons',
+      'lodash-es',
+    ],
+  },
   logging: {
     incomingRequests: false, // 关闭请求日志
   },
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "**",
+        protocol: 'http',
+        hostname: '**',
       },
       {
-        protocol: "https",
-        hostname: "**",
+        protocol: 'https',
+        hostname: '**',
       },
     ],
     dangerouslyAllowSVG: true,
   },
-  transpilePackages: ["next-mdx-remote"],
-};
+  transpilePackages: ['next-mdx-remote'],
+}
 
-export default nextConfig;
+export default nextConfig
